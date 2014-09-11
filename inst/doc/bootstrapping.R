c174b0408af1476367753d4cfae57947ebf77c16
dgrtwo <dgrtwo@princeton.edu>
2014-09-11 15:56:58 -0400

    Changed bootstrapping vignette to take advantage of [this StackOverflow answer](http://stackoverflow.com/questions/25793383/using-dplyrs-do-to-perform-bootstrap-replications)

--- inst/doc/bootstrapping.R
+++ inst/doc/bootstrapping.R
@@ -13,14 +13,27 @@ summary(nlsfit)
 ggplot(mtcars, aes(wt, mpg)) + geom_point() + geom_line(aes(y=predict(nlsfit)))
 
 ## ------------------------------------------------------------------------
-library(dplyr)
-set.seed(2014)
-bootreps <- data.frame(replication=1:100) %>% group_by(replication) %>%
-    do(sample_n(mtcars, nrow(mtcars), replace=TRUE))
+bootstrap <- function(df, m) {
+  n <- nrow(df)
+
+  attr(df, "indices") <- replicate(m, sample(n, replace = TRUE), 
+                                   simplify = FALSE)
+  attr(df, "drop") <- TRUE
+  attr(df, "group_sizes") <- rep(n, m)
+  attr(df, "biggest_group_size") <- n
+  attr(df, "labels") <- data.frame(replicate = 1:m)
+  attr(df, "vars") <- list(quote(replicate))
+  class(df) <- c("grouped_df", "tbl_df", "tbl", "data.frame")
+
+  df
+}
 
 ## ------------------------------------------------------------------------
+library(dplyr)
 library(broom)
-bootnls <- bootreps %>% do(tidy(nls(mpg ~ k / wt + b, ., start=list(k=1, b=0))))
+bootnls <- bootstrap(mtcars, 100) %>% do(tidy(nls(mpg ~ k / wt + b, ., start=list(k=1, b=0))))
+
+## ------------------------------------------------------------------------
 bootnls
 
 ## ------------------------------------------------------------------------
