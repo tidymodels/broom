343b34a15ef010b94ea87f9106dacf809f2693d9
dgrtwo <dgrtwo@princeton.edu>
2014-10-16 12:20:09 -0400

    Changed augment.lm to include .fitted.response and .se.fit.response for glm objects.
    Added further tests for lm and glm objects.

--- tests/testthat/helper-checkers.R
+++ tests/testthat/helper-checkers.R
@@ -1,5 +1,14 @@
-check_tidy <- function(o, exp.row = NULL, exp.col = NULL, exp.names = NULL) {
+#' test the basics of tidy/augment/glance output: is a data frame, no row names
+check_tidiness <- function(o) {
     expect_is(o, "data.frame")
+    expect_equal(rownames(o), as.character(seq_len(nrow(o))))
+}
+
+
+#' check the output of a tidy function
+check_tidy <- function(o, exp.row = NULL, exp.col = NULL, exp.names = NULL) {
+    check_tidiness(o)
+
     if (!is.null(exp.row)) {
         expect_equal(nrow(o), exp.row)
     }
@@ -9,7 +18,21 @@ check_tidy <- function(o, exp.row = NULL, exp.col = NULL, exp.names = NULL) {
     if (!is.null(exp.names)) {
         expect_true(all(exp.names %in% colnames(o)))
     }
+}
+
+
+#' check the output of an augment function
+check_augment <- function(au, original = NULL, exp.names = NULL) {
+    check_tidiness(au)
     
-    # test that there are no rownames
-    expect_equal(rownames(o), as.character(seq_len(nrow(o))))
+    if (!is.null(original)) {
+        # check that all rows in original appear in output
+        expect_equal(nrow(au), nrow(original))
+        
+        # TODO: handle NAs etc
+    }
+    
+    if (!is.null(exp.names)) {
+        expect_true(all(exp.names %in% colnames(au)))
+    }
 }
