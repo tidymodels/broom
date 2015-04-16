dd406520f77296d075ed55313b1a48e3f7ebce5a
dgrtwo <dgrtwo@princeton.edu>
2015-04-16 16:36:04 -0400

    Added @param documentation, newdata argument, @name, and examples.

--- R/loess_tidiers.R
+++ R/loess_tidiers.R
@@ -4,19 +4,39 @@
 #' on the fitted values and residuals, and optionally the 
 #' standard errors.
 #' 
+#' @param x A "loess" object
+#' @param data Original data, defaults to the extracting it from the model
+#' @param newdata If provided, performs predictions on the new data
+#' @param ... extra arguments
+#' 
+#' @name loess_tidiers
+#' 
+#' @template augment_NAs
+#' 
 #' @return When \code{newdata} is not supplied \code{augment.loess}
 #' returns one row for each observation with three columns added
-#' to the original data (two if \code{se = FALSE}):
+#' to the original data:
 #'    \item{.fitted}{Fitted values of model}
 #'    \item{.se.fit}{Standard errors of the fitted values}
 #'    \item{.resid}{Residuals of the fitted values}
 #'    
-#'    When \code{newdata} is supplied \code{augment.loess} returns
-#'    one row for each observation with one additional column (two
-#'    if \code{se = TRUE}):
+#' When \code{newdata} is supplied \code{augment.loess} returns
+#'    one row for each observation with one additional column:
 #'    \item{.fitted}{Fitted values of model}
 #'    \item{.se.fit}{Standard errors of the fitted values}
+#' 
+#' @examples
+#' 
+#' lo <- loess(mpg ~ wt, mtcars)
+#' augment(lo)
+#' 
+#' # with all columns of original data
+#' augment(lo, mtcars)
+#' 
+#' # with a new dataset
+#' augment(lo, newdata = head(mtcars))
+#' 
 #' @export
-augment.loess <- function(x, data = model.frame(x), se = TRUE,...){
-    augment_columns(x, data, se.fit = FALSE, se = se,...)
+augment.loess <- function(x, data = model.frame(x), newdata, ...){
+    augment_columns(x, data, newdata, se.fit = FALSE, se = TRUE, ...)
 }
