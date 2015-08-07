db5b84f682d9d4e664d2691f7dd88447aed951f8
dgrtwo <dgrtwo@princeton.edu>
2015-08-07 00:45:38 -0400

    Few tweaks to rq tidiers, mainly moving rq_process from its own file to rq_tidiers.R.

--- R/rq_tidiers.R
+++ R/rq_tidiers.R
@@ -5,8 +5,7 @@
 #' on the fitted values and residuals, and construct a glance of
 #' the model's statistics.
 #' 
-#' @return All tidying methods return a \code{data.frame} without rownammes.
-#' The structure depends on the method chosen.
+#' @template boilerplate
 #' 
 #' @name rq_tidiers
 #' 
@@ -242,3 +241,33 @@ augment.rqs <- function(x,data = model.frame(x), newdata, ...){
 #' 
 #' @export
 augment.nlrq <- augment.nls
+
+
+#' Helper function for tidy.rq and tidy.rqs
+#' 
+#' See documentation for \code{summary.rq} for complete description
+#' of the options for \code{se.type}, \code{conf.int}, etc.
+#' 
+#' @param rq_obj an object returned by \code{summary.rq} or \code{summary.rqs}
+#' @param se.type type of standard errors used in \code{summary.rq} or \code{summary.rqs}
+#' @param conf.int
+#' @param conf.level
+#' @param \dots currently unused
+process_rq <- function(rq_obj,se.type = "rank",conf.int = TRUE, conf.level = 0.95,...){
+    nn <- c("estimate", "std.error", "statistic", "p.value")
+    co <- as.data.frame(rq_obj[["coefficients"]])
+    if (se.type == "rank"){
+        co <- setNames(co,c("estimate","conf.low","conf.high"))
+        conf.int <- FALSE
+    }else{
+        co <- setNames(co,nn)
+    }
+    if (conf.int){
+        a <- (1 - conf.level) / 2
+        cv <- qt(c(a,1-a),df = rq_obj[["rdf"]])
+        co[["conf.low"]] <- co[["estimate"]] + (cv[1] * co[["std.error"]])
+        co[["conf.high"]] <- co[["estimate"]] + (cv[2] * co[["std.error"]])
+    }
+    co[["tau"]] <- rq_obj[["tau"]]
+    fix_data_frame(co,colnames(co))
+}
