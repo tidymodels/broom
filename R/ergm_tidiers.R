8a849cfaa382e3507bf0780bf849532f9909580b
dgrtwo <dgrtwo@princeton.edu>
2015-08-13 21:43:49 -0400

    Added .Rd files for ergm tidiers

--- R/ergm_tidiers.R
+++ R/ergm_tidiers.R
@@ -25,8 +25,6 @@
 #'
 #' if (require("ergm")) {
 #'     # Using the same example as the ergm package
-#'     library(ergm)
-#'
 #'     # Load the Florentine marriage network data
 #'     data(florentine)
 #'
