134ef11d72d9b3e742911fda82310670658c1a56
Dave Robinson <drobinson@stackoverflow.com>
2015-09-08 15:07:24 -0400

    Fixed some issues with column names in tidy.rcorr

--- R/rcorr_tidiers.R
+++ R/rcorr_tidiers.R
@@ -7,28 +7,28 @@
 #' with one row for each pair of columns being compared.
 #' 
 #' @param x An object of class "rcorr"
-#' @param names Optionally, the column names of the original
-#' matrix, to use to fill in these columns.
 #' @param diagonal Whether to include diagonal elements (where
 #' \code{estimate} is 1 and \code{p.value} is NA), default FALSE
 #' @param ... extra arguments (not used)
 #' 
 #' @return A data.frame with one row for each pairing
 #' in the correlation matrix. Columns are:
-#'   \item{column1}{The first column being described. A number, unless
-#'   \code{names} is provided, in which case it will be the name}
-#'   \item{column2}{The second column being described. A number, unless
-#'   \code{names} is provided, in which case it will be the name}
+#'   \item{column1}{Name or index of the first column being described}
+#'   \item{column2}{Name or index of the second column being described}
 #'   \item{estimate}{Estimate of Pearson's r or Spearman's rho}
 #'   \item{n}{Number of observations used to compute the correlation}
 #'   \item{p.value}{P-value of correlation}
 #' 
+#' @details Only half the symmetric matrix is shown.
+#' 
 #' @examples
 #' 
 #' if (require("Hmisc", quietly = TRUE)) {
-#'     mat <- replicate(100, rnorm(100))
+#'     mat <- replicate(52, rnorm(100))
 #'     # add some NAs
 #'     mat[sample(length(mat), 2000)] <- NA
+#'     # also column names
+#'     colnames(mat) <- c(LETTERS, letters)
 #'     
 #'     rc <- rcorr(mat)
 #'     
@@ -47,19 +47,14 @@
 #' @name rcorr_tidiers
 #' 
 #' @export
-tidy.rcorr <- function(x, names = NULL, diagonal = FALSE, ...) {
-    if (!is.null(names)) {
-        rownames(x$r) <- colnames(x$r) <- names
-    }
-    
+tidy.rcorr <- function(x, diagonal = FALSE, ...) {
     ret <- reshape2::melt(x$r, varnames = c("column1", "column2"),
                           value.name = "estimate")
     ret$n <- c(x$n)
     ret$p.value <- c(x$P)
     
-    if (!diagonal) {
-        ret <- filter(ret, column1 != column2)
-    }
-    
+    # include only half the symmetric matrix.
+    ret <- ret[upper.tri(x$r, diag = diagonal), ]
+
     ret
 }
