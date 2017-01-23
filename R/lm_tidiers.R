132e3735d7c3b7b67e5e491aea4cefba6fa2706f
Matteo Sostero <matteo.sostero@gmail.com>
2017-01-23 16:41:00 +0100

    tidy.lm with quick=T now returns coefficients as character, nor factor.

--- R/lm_tidiers.R
+++ R/lm_tidiers.R
@@ -132,7 +132,7 @@ tidy.lm <- function(x, conf.int = FALSE, conf.level = .95,
                     exponentiate = FALSE, quick = FALSE, ...) {
     if (quick) {
         co <- stats::coef(x)
-        ret <- data.frame(term = names(co), estimate = unname(co))
+        ret <- data.frame(term = names(co), estimate = unname(co), stringsAsFactors = FALSE)
         return(process_lm(ret, x, conf.int = FALSE, exponentiate = exponentiate))
     }
     s <- summary(x)
