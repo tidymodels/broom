27c3e06ad98272d4d3c5ef4959417cf9fc5203fb
Aaron Jacobs <atheriel@gmail.com>
2017-02-19 13:22:23 -0500

    Add tests for the new tidy.ts implementation.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

--- tests/testthat/test-tidy.R
+++ tests/testthat/test-tidy.R
@@ -90,3 +90,19 @@ test_that("tidy.summary works (even with NAs)", {
     expect_equal(td$maximum, 14)
     expect_equal(td$na, 3)
 })
+
+context("tidying time series")
+
+test_that("tidy.ts works for univariate series", {
+    x <- ts(1:10, frequency = 4, start = c(1959, 2))
+    tx <- tidy(x)
+    check_tidiness(tx)
+    check_tidy(tx, exp.names = c("index", "value"))
+})
+
+test_that("tidy.ts works for multivariate series", {
+    z <- ts(matrix(rnorm(300), 100, 3), start = c(1961, 1), frequency = 12)
+    tz <- tidy(z)
+    check_tidiness(tz)
+    check_tidy(tz, exp.names = c("index", "series", "value"))
+})
