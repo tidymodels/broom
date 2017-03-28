5e5b720a0b4f95aefd9214a9d58018d755573b1b
kevinykuo <kevinykuo@gmail.com>
2017-03-28 18:46:07 -0400

    initial h2o support

---
+++ R/h2o_glm_tidiers.R
@@ -0,0 +1,53 @@
+tidy_h2o_glm <- function(x, exponentiate = FALSE) {
+    newnames <- if (x@allparameters$compute_p_values) {
+        c("term", "estimate", "std.error", "statistic", "p.value",
+          "standardized_coefficients")
+    } else {
+        c("term", "estimate", "standardized_coefficients")
+    }
+    
+    if (exponentiate) {
+        if (x@allparameters$link != "logit" && x@allparameters$link  != "log") {
+            warning(paste("Exponentiating coefficients, but model did not use",
+                          "a log or logit link function"))
+        }
+        trans <- exp
+    } else {
+        trans <- identity
+    }
+    
+    x@model$coefficients_table %>%
+        fix_data_frame(newnames = newnames) %>%
+        purrr::modify_at(c("estimate", "standardized_coefficients"), trans)
+}
+
+augment_h2o_glm <- function(x,
+                            data = h2o.getFrame(x@allparameters$training_frame),
+                            newdata, collect, ...) {
+    
+    if (!(is.null(newdata) || is.h2o(newdata)))
+        stop("newdata must be a H2OFrame")
+    stopifnot(is.logical(collect))
+    if (! class(x) %in% c("H2ORegressionModel", "H2OBinomialModel"))
+        stop(paste0(class(x), " not supported"))
+    
+    ret <- if (is.null(newdata)) data else newdata
+    pred <- predict(x, ret)
+    if (class(x) == "H2OBinomialModel") {
+        ret$.fitted <- data[[x@allparameters$y]] %>%
+            h2o.levels %>%
+            .[2] %>%
+            pred[.]
+    } else {
+        ret$.fitted <- pred$predict
+    }
+    if (x@allparameters$compute_p_values) {
+        ret$.se.fit <- pred$StdErr
+    }
+    
+    if (collect == TRUE) {
+        as.data.frame(ret)
+    } else {
+        ret
+    }
+}
