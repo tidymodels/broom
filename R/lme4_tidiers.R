5debfecaa3b915d283c0abb28254dca9ffd1ceeb
Ben Bolker <bbolker@gmail.com>
2017-08-20 15:09:05 -0400

    fix augment example

--- R/lme4_tidiers.R
+++ R/lme4_tidiers.R
@@ -347,31 +347,32 @@ glance.merMod <- function(x, ...) {
 ##' @importFrom reshape2 melt
 ##' @importFrom plyr ldply
 ##' @examples
-##' if (require("lme4"))
-##' fit <- lmer(Reaction~Days+(Days|Subject),sleepstudy)
-##' rr <- ranef(fit,condVar=TRUE)
-##' aa <- augment(rr)
-##' ## Q-Q plot:
-##' if (require(ggplot2)) {
-##'    g0 <- ggplot(aa,aes(estimate,qq,xmin=lb,xmax=ub))+
-##'       geom_errorbarh(height=0)+
-##'       geom_point()+facet_wrap(~variable,scale="free_x")
-##'    ## regular caterpillar plot:
-##'    g1 <- ggplot(aa,aes(estimate,level,xmin=lb,xmax=ub))+
-##'       geom_errorbarh(height=0)+
-##'       geom_vline(xintercept=0,lty=2)+
-##'       geom_point()+facet_wrap(~variable,scale="free_x")
-##'    ## emphasize extreme values
-##'    aa2 <- ddply(aa,c("grp","level"),
-##'             transform,
-##'             keep=any(estimate/std.error>2))
-##'    aa3 <- subset(aa2,keep)
-##'    ## Update caterpillar plot with extreme levels highlighted:
-##'    ggplot(aa2,aes(estimate,level,xmin=lb,xmax=ub,colour=factor(keep)))+
-##'       geom_errorbarh(height=0)+
-##'       geom_vline(xintercept=0,lty=2)+
-##'       geom_point()+facet_wrap(~variable,scale="free_x")+
-##'       scale_colour_manual(values=c("black","red"),guide=FALSE)
+##' if (require("lme4")) {
+##'    fit <- lmer(Reaction~Days+(Days|Subject),sleepstudy)
+##'    rr <- ranef(fit,condVar=TRUE)
+##'    aa <- augment(rr)
+##'    ## Q-Q plot:
+##'    if (require(ggplot2) && require(dplyr)) {
+##'       g0 <- ggplot(aa,aes(estimate,qq,xmin=lb,xmax=ub))+
+##'           geom_errorbarh(height=0)+
+##'           geom_point()+facet_wrap(~variable,scale="free_x")
+##'       ## regular caterpillar plot:
+##'       g1 <- ggplot(aa,aes(estimate,level,xmin=lb,xmax=ub))+
+##'          geom_errorbarh(height=0)+
+##'          geom_vline(xintercept=0,lty=2)+
+##'          geom_point()+facet_wrap(~variable,scale="free_x")
+##'       ## emphasize extreme values
+##'       aa2 <- aa  %>% group_by(grp,level) %>%
+##'             mutate(keep=any(estimate/std.error>2))
+##'        ## Update caterpillar plot with extreme levels highlighted
+##'        ##  (highlight all groups with *either* extreme intercept *or*
+##'        ##   extreme slope)
+##'       ggplot(aa2,aes(estimate,level,xmin=lb,xmax=ub,colour=factor(keep)))+
+##'          geom_errorbarh(height=0)+
+##'          geom_vline(xintercept=0,lty=2)+
+##'          geom_point()+facet_wrap(~variable,scale="free_x")+
+##'          scale_colour_manual(values=c("black","red"), guide=FALSE)
+##'    }
 ##' }
 ##' @importFrom stats ppoints
 ##' @export 
