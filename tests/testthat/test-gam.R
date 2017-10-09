797573de508573f3f07cd9160a18d6536e28bec6
David Hugh-Jones <davidhughjones@gmail.com>
2017-10-09 21:26:17 +0100

    Revert "add tests for mgcv::gam"
    This reverts commit c9f3f8fde42d30618c16898976d4d8a9d6aeefd0.

--- tests/testthat/test-gam.R
+++ tests/testthat/test-gam.R
@@ -1,25 +1,6 @@
 if (requireNamespace("gam")) {
-    context("gam models from package gam")
     data(kyphosis, package = "gam")
     g <- gam::gam(Kyphosis ~ gam::s(Age,4) + Number, family = binomial, data = kyphosis)
-    test_that("tidy works on gam models", {
-        tidy(g)    
-    })
-    test_that("glance works on gam models", {
-        glance(g)    
-    })
+    
+    td <- tidy(g)
 }
-
-if (requireNamespace("mgcv")) {
-    context("gam models from package mgcv")
-    d <- as.data.frame(ChickWeight)
-    g <- mgcv::gam(weight ~ s(Time) + factor(Diet), data = d)
-    test_that("tidy works on mgcv::gam models", {
-        tidy(g) 
-        tidy(g, parametric = TRUE) 
-    })
-    test_that("glance works on mgcv::gam models", {
-        glance(g)
-    })
-}
-
