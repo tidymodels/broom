cb338456e056a3eb332631aefa2dfe26278ed731
Derek Chiu <dchiu@bccrc.ca>
2017-11-15 11:51:32 -0800

    fix gam tests to be not empty

--- tests/testthat/test-gam.R
+++ tests/testthat/test-gam.R
@@ -1,25 +1,33 @@
-if (requireNamespace("gam")) {
-    context("gam models from package gam")
-    data(kyphosis, package = "gam")
-    g <- gam::gam(Kyphosis ~ gam::s(Age,4) + Number, family = binomial, data = kyphosis)
-    test_that("tidy works on gam models", {
-        tidy(g)    
-    })
-    test_that("glance works on gam models", {
-        glance(g)    
-    })
-}
+context("gam models from package gam")
 
-if (requireNamespace("mgcv")) {
-    context("gam models from package mgcv")
-    d <- as.data.frame(ChickWeight)
-    g <- mgcv::gam(weight ~ s(Time) + factor(Diet), data = d)
-    test_that("tidy works on mgcv::gam models", {
-        tidy(g) 
-        tidy(g, parametric = TRUE) 
-    })
-    test_that("glance works on mgcv::gam models", {
-        glance(g)
-    })
-}
+library(gam)
+
+test_that("tidy and glance work on gam models", {
+    data(kyphosis)
+    g <- gam::gam(Kyphosis ~ gam::s(Age, 4) + Number, family = binomial,
+                  data = kyphosis)
+    td <- tidy(g)
+    gl <- glance(g)
+    
+    check_tidy(td, exp.row = 3, exp.col = 6)
+    check_tidy(gl, exp.col = 6)
+})
+
+
+context("gam models from package mgcv")
+
+library(mgcv)
+
+test_that("tidy and glance work on mgcv::gam models", {
+    set.seed(2)
+    dat <- gamSim(1, n = 400, dist = "normal", scale = 2, verbose = FALSE)
+    b <- mgcv::gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat)
+    td <- tidy(b)
+    tdp <- tidy(b, parametric = TRUE)
+    gl <- glance(b)
+    
+    check_tidy(td, exp.row = 4, exp.col = 5)
+    check_tidy(tdp, exp.col = 5)
+    check_tidy(gl, exp.col = 6)
+})
 
