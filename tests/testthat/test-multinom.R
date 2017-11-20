b81fd0edd5668926a17d22c08d350a4bf9b9fda3
Derek Chiu <dchiu@bccrc.ca>
2017-11-20 09:56:40 -0800

    faster and suppress convergence messages

--- tests/testthat/test-multinom.R
+++ tests/testthat/test-multinom.R
@@ -3,9 +3,9 @@ context("multinom tidiers")
 library(nnet)
 
 test_that("multinom tidiers work", {
-    bwt.mu <- multinom(low ~ ., bwt)
-    td <- tidy(bwt.mu, conf.int = TRUE)
-    check_tidy(td, exp.row = 11, exp.col = 8)
+    fit.gear <- multinom(gear ~ mpg + factor(am), data = mtcars, trace = FALSE)
+    td <- tidy(fit.gear, conf.int = TRUE)
+    check_tidy(td, exp.row = 6, exp.col = 8)
     
     gl <- glance(bwt.mu)
     check_tidy(gl, exp.col = 3)
