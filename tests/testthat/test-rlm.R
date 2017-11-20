abd8d82250ba86a5ef07301db1ac084386dcd7f1
Derek Chiu <dchiu@bccrc.ca>
2017-11-20 14:32:20 -0800

    add tests for psych, ridgelm, rlm, robust, rq

---
+++ tests/testthat/test-rlm.R
@@ -0,0 +1,11 @@
+context("rlm tidiers")
+
+library(MASS)
+
+test_that("tidy and augment methods use tidy.lm, glance works", {
+    r <- rlm(stack.loss ~ ., stackloss)
+    expect_identical(tidy(r), tidy.lm(r))
+    expect_identical(augment(r), augment.lm(r))
+    gl <- glance(r)
+    check_tidy(gl, exp.col = 6)
+})
