4f4016e97ebef9eb241be382fb70babc107aad25
Derek Chiu <dchiu@bccrc.ca>
2017-11-28 17:15:34 -0800

    fix dependencies in sp and zoo

--- tests/testthat/test-sp.R
+++ tests/testthat/test-sp.R
@@ -1,6 +1,6 @@
 context("sp tidiers")
 
-library(maptools)
+library(sp)
 
 test_that("polygon tidiers work", {
     # tidy.Polygon
@@ -8,24 +8,24 @@ test_that("polygon tidiers work", {
     Sr2 <- Polygon(cbind(c(5, 4, 2, 5), c(2, 3, 2, 2)))
     Sr3 <- Polygon(cbind(c(4, 4, 5, 10, 4), c(5, 3, 2, 5, 5)))
     Sr4 <- Polygon(cbind(c(5, 6, 6, 5, 5), c(4, 4, 3, 3, 4)), hole = TRUE)
-    
+
     td <- tidy(Sr1)
     check_tidy(td, exp.row = 5, exp.col = 4)
-    
+
     # tidy.Polygons
     Srs1 <- Polygons(list(Sr1), "s1")
     Srs2 <- Polygons(list(Sr2), "s2")
     Srs3 <- Polygons(list(Sr3, Sr4), "s3/4")
-    
+
     td <- tidy(Srs1)
     check_tidy(td, exp.row = 5, exp.col = 7)
-    
+
     # tidy.SpatialPolygons
     SpP <- SpatialPolygons(list(Srs1, Srs2, Srs3), 1:3)
-    
+
     td <- tidy(SpP)
     check_tidy(td, exp.row = 19, exp.col = 7)
-    
+
     # tidy.SpatialPolygonsDataFrame
     grd <- GridTopology(c(1, 1), c(1, 1), c(10, 10))
     polys <- as(grd, "SpatialPolygons")
@@ -37,10 +37,10 @@ test_that("polygon tidiers work", {
         polys,
         data = data.frame(x = x, y = y, z = z, row.names = row.names(polys))
     )
-    
+
     td <- tidy(SpPDF)
     check_tidy(td, exp.row = 500, exp.col = 7)
-    
+
     td <- tidy(SpPDF, region = "x")
     check_tidy(td, exp.row = 230, exp.col = 7)
 })
@@ -52,29 +52,29 @@ test_that("line tidiers work", {
     rownames(l1a) <- letters[1:3]
     l2 <- cbind(c(1, 2, 3), c(1, 1.5, 1))
     rownames(l2) <- letters[1:3]
-    
+
     # tidy.Line
     Sl1 <- Line(l1)
     Sl1a <- Line(l1a)
     Sl2 <- Line(l2)
-    
+
     td <- tidy(Sl1)
     check_tidy(td, exp.row = 3, exp.col = 3)
-    
+
     # tidy.Lines
     S1 <- Lines(list(Sl1, Sl1a), ID = "a")
     S2 <- Lines(list(Sl2), ID = "b")
-    
+
     td <- tidy(S1)
     check_tidy(td, exp.row = 6, exp.col = 6)
-    
+
     # tidy.SpatialLinesDataFrame
     Sl <- SpatialLines(list(S1, S2))
     SlDF <- SpatialLinesDataFrame(
         Sl,
         data = data.frame(sp = c(5, 10), row.names = c("a", "b"))
     )
-    
+
     td <- tidy(SlDF)
     check_tidy(td, exp.row = 9, exp.col = 6)
 })
--- tests/testthat/test-zoo.R
+++ tests/testthat/test-zoo.R
@@ -1,14 +1,12 @@
 context("zoo tidiers")
 
-library(zoo)
-
 test_that("tidy.zoo works", {
     set.seed(1071)
-    Z.index <- as.Date(sample(12450:12500, 10))
+    Z.index <- zoo::as.Date(sample(12450:12500, 10))
     Z.data <- matrix(rnorm(30), ncol = 3)
     colnames(Z.data) <- c("Aa", "Bb", "Cc")
-    Z <- zoo(Z.data, Z.index)
-    
+    Z <- zoo::zoo(Z.data, Z.index)
+
     td <- tidy(Z)
     check_tidy(td, exp.row = 30, exp.col = 3)
 })
