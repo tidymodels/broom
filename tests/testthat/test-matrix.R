3ad4b992a6a9ae20d5161ea90d48882443d9c262
Derek Chiu <dchiu@bccrc.ca>
2017-11-30 14:35:30 -0800

    did not update data source

--- tests/testthat/test-matrix.R
+++ tests/testthat/test-matrix.R
@@ -4,7 +4,7 @@ test_that("matrix tidiers work", {
     mat <- as.matrix(mtcars)
     
     td <- tidy(mat)
-    check_tidy(td, exp.row = 150, exp.col = 4)
+    check_tidy(td, exp.row = 32, exp.col = 12)
     
     gl <- glance(mat)
     check_tidy(gl, exp.col = 4)
