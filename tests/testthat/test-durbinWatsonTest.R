a30d70f19bf6cfbb7be101313da34bf1e60e247c
Michael Kuehn <mkuehn10@gmail.com>
2018-06-09 17:38:58 -0400

    add car package to DESCRIPTION Suggests

--- tests/testthat/test-durbinWatsonTest.R
+++ tests/testthat/test-durbinWatsonTest.R
@@ -1,12 +1,12 @@
 context("durbinWatsonTest tidiers")
 
 test_that("tidy.durbinWatsonTest same as glance.durbinWatsonTest", {
-  dw <- durbinWatsonTest(lm(mpg ~ wt, data = mtcars))
+  dw <- car::durbinWatsonTest(lm(mpg ~ wt, data = mtcars))
   expect_identical(tidy(dw), glance(dw))
 })
 
 test_that("tidy.durbinWatsonTest works", {
-  dw <- durbinWatsonTest(lm(mpg ~ wt, data = mtcars))
+  dw <- car::durbinWatsonTest(lm(mpg ~ wt, data = mtcars))
   td <- tidy(dw)
   check_tidy(td, exp.col = 5, exp.row = 1)
 })
