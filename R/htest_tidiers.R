c24b4756430276cab095b3cc63d55f94f117011e
Benjamin Nutter <benjamin.nutter@gmail.com>
2018-06-14 15:37:13 +0000

    remove straighten, See #2

--- R/htest_tidiers.R
+++ R/htest_tidiers.R
@@ -54,31 +54,34 @@ tidy.htest <- function(x, ...) {
     
     # estimate may have multiple values
     if (length(ret$estimate) > 1) {
-        names(ret$estimate) <- paste0("estimate", seq_along(ret$estimate))
-        ret <- c(ret$estimate, ret)
-        ret$estimate <- NULL
-        
-        # special case: in a t-test, estimate = estimate1 - estimate2
-        if (x$method == "Welch Two Sample t-test") {
-            ret <- c(estimate=ret$estimate1 - ret$estimate2, ret)
-        }
+      names(ret$estimate) <- paste0("estimate", seq_along(ret$estimate))
+      ret <- c(ret$estimate, ret)
+      ret$estimate <- NULL
+      
+      # special case: in a t-test, estimate = estimate1 - estimate2
+      if (grepl("Two Sample t-test", x$method)) {
+        group_names <- sub("^mean (of|in group) ", "", names(x$estimate))
+        ret <- c(group1 = group_names[1],
+                 group2 = group_names[2],
+                 estimate=ret$estimate1 - ret$estimate2, ret)
+      }
     }
     
     # parameter may have multiple values as well, such as oneway.test
     if (length(x$parameter) > 1) {
-        ret$parameter <- NULL
-        if (is.null(names(x$parameter))) {
-            warning("Multiple unnamed parameters in hypothesis test; dropping them")
-        } else {
-            message("Multiple parameters; naming those columns ",
-                    paste(make.names(names(x$parameter)), collapse = ", "))
-            ret <- append(ret, x$parameter, after = 1)
-        }
+      ret$parameter <- NULL
+      if (is.null(names(x$parameter))) {
+        warning("Multiple unnamed parameters in hypothesis test; dropping them")
+      } else {
+        message("Multiple parameters; naming those columns ",
+                paste(make.names(names(x$parameter)), collapse = ", "))
+        ret <- append(ret, x$parameter, after = 1)
+      }
     }
     
     ret <- compact(ret)
     if (!is.null(x$conf.int)) {
-        ret <- c(ret, conf.low=x$conf.int[1], conf.high=x$conf.int[2])
+      ret <- c(ret, conf.low=x$conf.int[1], conf.high=x$conf.int[2])
     }
     if (!is.null(x$method)) {
       ret <- c(ret, method = as.character(x$method))
@@ -86,7 +89,7 @@ tidy.htest <- function(x, ...) {
     if (!is.null(x$alternative)) {
       ret <- c(ret, alternative = as.character(x$alternative))
     }
-    unrowname(as.data.frame(ret))
+    unrowname(tibble::as_data_frame(ret))
 }
 
 
