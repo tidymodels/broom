065c24591965298f37dcecabfb10de0f1db7a178
Patrick Kennedy <ppaine@uoregon.edu>
2018-08-03 16:35:59 -0700

    Unprotect examples
    Closes #439

--- R/lme4-tidiers.R
+++ R/lme4-tidiers.R
@@ -16,32 +16,31 @@
 #' @examples
 #'
 #' \dontrun{
-#' if (require("lme4")) {
-#'     # example regressions are from lme4 documentation
-#'     lmm1 <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
-#'     tidy(lmm1)
-#'     tidy(lmm1, effects = "fixed")
-#'     tidy(lmm1, effects = "fixed", conf.int=TRUE)
-#'     tidy(lmm1, effects = "fixed", conf.int=TRUE, conf.method="profile")
-#'     tidy(lmm1, effects = "ran_modes", conf.int=TRUE)
-#'     head(augment(lmm1, sleepstudy))
-#'     glance(lmm1)
+#'   library(lme4)
+#'   # example regressions are from lme4 documentation
+#'   lmm1 <- lmer(Reaction ~ Days + (Days | Subject), sleepstudy)
+#'   tidy(lmm1)
+#'   tidy(lmm1, effects = "fixed")
+#'   tidy(lmm1, effects = "fixed", conf.int=TRUE)
+#'   tidy(lmm1, effects = "fixed", conf.int=TRUE, conf.method="profile")
+#'   tidy(lmm1, effects = "ran_modes", conf.int=TRUE)
+#'   head(augment(lmm1, sleepstudy))
+#'   glance(lmm1)
 #'
-#'     glmm1 <- glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
-#'                   data = cbpp, family = binomial)
-#'     tidy(glmm1)
-#'     tidy(glmm1, effects = "fixed")
-#'     head(augment(glmm1, cbpp))
-#'     glance(glmm1)
+#'   glmm1 <- glmer(cbind(incidence, size - incidence) ~ period + (1 | herd),
+#'                  data = cbpp, family = binomial)
+#'   tidy(glmm1)
+#'   tidy(glmm1, effects = "fixed")
+#'   head(augment(glmm1, cbpp))
+#'   glance(glmm1)
 #'
-#'     startvec <- c(Asym = 200, xmid = 725, scal = 350)
-#'     nm1 <- nlmer(circumference ~ SSlogis(age, Asym, xmid, scal) ~ Asym|Tree,
-#'                   Orange, start = startvec)
-#'     tidy(nm1)
-#'     tidy(nm1, effects = "fixed")
-#'     head(augment(nm1, Orange))
-#'     glance(nm1)
-#' }
+#'   startvec <- c(Asym = 200, xmid = 725, scal = 350)
+#'   nm1 <- nlmer(circumference ~ SSlogis(age, Asym, xmid, scal) ~ Asym|Tree,
+#'                Orange, start = startvec)
+#'   tidy(nm1)
+#'   tidy(nm1, effects = "fixed")
+#'   head(augment(nm1, Orange))
+#'   glance(nm1)
 #' }
 NULL
 
