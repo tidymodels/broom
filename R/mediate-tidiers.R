e563030275d648cad35d6ea3b947639f5e37e100
Lily Medina <lilymiru@gmail.com>
2018-10-25 18:46:32 +0200

    save object as tibble and increase coverage
    - this changes columns' type from chr to dbl
    - increase coverage of tidy.mediate() to 100

--- R/mediate-tidiers.R
+++ R/mediate-tidiers.R
@@ -31,32 +31,31 @@ tidy.mediate <- function(x, conf.int = FALSE, conf.level = .95, ...){
   sims <- s$sims
   ci <- NULL
   co <- with(s, 
-             cbind( c("acme_0", "acme_1", "ade_0", "ade_1"), 
-                    c(d0, d1, z0, z1),
-                    c(sd(d0.sims), sd(d1.sims) , sd(z0.sims), sd(z1.sims)),
-                    c(d0.p, d1.p, z0.p, z1.p)))
+             tibble( c("acme_0", "acme_1", "ade_0", "ade_1"), 
+                     c(d0, d1, z0, z1),
+                     c(sd(d0.sims), sd(d1.sims) , sd(z0.sims), sd(z1.sims)),
+                     c(d0.p, d1.p, z0.p, z1.p)))
   
   
   if(conf.int){
     low <- (1 - conf.level)/2
     high <- 1 - low
-    if(s$boot.ci.type == "bca"){
-      BC.CI <- function(theta){
-        z.inv <- length(theta[theta < mean(theta)])/sims
-        z <- qnorm(z.inv)
-        U <- (sims - 1) * (mean(theta) - theta)
-        top <- sum(U^3)
-        under <- 6 * (sum(U^2))^{3/2}
-        a <- top / under
-        lower.inv <-  pnorm(z + (z + qnorm(low))/(1 - a * (z + qnorm(low))))
-        lower2 <- lower <- quantile(theta, lower.inv)
-        upper.inv <-  pnorm(z + (z + qnorm(high))/(1 - a * (z + qnorm(high))))
-        upper2 <- upper <- quantile(theta, upper.inv)
-        return(c(lower, upper))      
-      }
-      ci <- with(x,
-                 sapply(list(d0.sims, d1.sims, z0.sims, z1.sims), function(x)  apply(x, 1, BC.CI)))
-    } else {
+    BC.CI <- function(theta){
+      z.inv <- length(theta[theta < mean(theta)])/sims
+      z <- qnorm(z.inv)
+      U <- (sims - 1) * (mean(theta) - theta)
+      top <- sum(U^3)
+      under <- 6 * (sum(U^2))^{3/2}
+      a <- top / under
+      lower.inv <-  pnorm(z + (z + qnorm(low))/(1 - a * (z + qnorm(low))))
+      lower2 <- lower <- quantile(theta, lower.inv)
+      upper.inv <-  pnorm(z + (z + qnorm(high))/(1 - a * (z + qnorm(high))))
+      upper2 <- upper <- quantile(theta, upper.inv)
+      return(c(lower, upper))      
+    }
+    ci <- with(x,
+               sapply(list(d0.sims, d1.sims, z0.sims, z1.sims), function(x)  apply(x, 1, BC.CI)))
+    if(s$boot.ci.type != "bca"){
       CI <- function(theta){
         return(quantile(theta, c(low, high), na.rm = TRUE))
       }
@@ -70,8 +69,7 @@ tidy.mediate <- function(x, conf.int = FALSE, conf.level = .95, ...){
   } 
   
   ret <- fix_data_frame(co, nn[1:ncol(co)])
-
+  
   
   as_tibble(ret)
 }
-
