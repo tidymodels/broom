4dc7eed2b1682068e124eb0f85903c44584f49ac
ltobalina <ltobalinas@gmail.com>
2018-11-05 22:14:11 +0100

    Add test for checking one-dimensional input to `Mclust()`.
    The case of giving a one-dimensional vector to `Mclust()` was missing.

--- tests/testthat/test-mclust.R
+++ tests/testthat/test-mclust.R
@@ -6,9 +6,11 @@ library(modeltests)
 skip_if_not_installed("mclust")
 library(mclust)
 dat <- iris[, 1:4]
+dat3 <- iris[, 1]
 
 fit <- Mclust(dat, G = 7, modelNames = "EII", verbose = FALSE)
 fit2 <- Mclust(dat, G = 1, verbose = FALSE)
+fit3 <- Mclust(dat3, G = 2, verbose = FALSE)
 
 test_that("mclust tidier arguments", {
   check_arguments(tidy.Mclust)
@@ -19,19 +21,23 @@ test_that("mclust tidier arguments", {
 test_that("tidy.Mclust", {
   td <- tidy(fit)
   td2 <- tidy(fit2)
+  td3 <- tidy(fit3)
   
   check_tidy_output(td, strict = FALSE)
   check_tidy_output(td2, strict = FALSE)
+  check_tidy_output(td3, strict = FALSE)
   
   check_dims(td, 7, 8)
   check_dims(td2, 1, 7)
+  check_dims(td3, 2, 5)
 })
 
 test_that("glance.Mclust", {
   gl <- glance(fit)
   gl2 <- glance(fit2)
+  gl3 <- glance(fit3)
   
-  check_glance_outputs(gl, gl2)
+  check_glance_outputs(gl, gl2, gl3)
 })
 
 test_that("augment.Mclust", {
@@ -48,5 +54,12 @@ test_that("augment.Mclust", {
     model = fit2,
     data = dat,
     newdata = dat
+  )
+  
+  check_augment_function(
+    aug = augment.Mclust,
+    model = fit3,
+    data = dat3,
+    newdata = dat3
   )
 })
