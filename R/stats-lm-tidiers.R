213f885ea8783c3c66c8d902a8b82aeae5f49abd
Matt Lehman <mhlehman24@gmail.com>
2018-11-28 09:44:16 -0500

    update documentation

--- R/stats-lm-tidiers.R
+++ R/stats-lm-tidiers.R
@@ -298,18 +298,51 @@ process_lm <- function(ret, x, conf.int = FALSE, conf.level = .95,
 }
 
 
-#' TODO: documentation
+# TODO: documentation
+
+#' @templateVar class lm.beta
+#' @template title_desc_tidy
+#' 
+#' @param x An `lm.beta` object created by [lm.beta::lm.beta].
+#' @template param_confint 
+#' @template param_quick
+#' @template param_exponentiate
+#' @template param_unused_dots
+#' 
+#' @evalRd return_tidy(regression = TRUE)
+#' 
+#' @details If the linear model is an `mlm` object (multiple linear model),
+#'   there is an additional column `response`.
+#' 
+#'   If you have missing values in your model data, you may need to refit
+#'   the model with `na.action = na.exclude`.
+#' 
+#' TODO: what is this:
 #' @evalRd return_tidy(regression = TRUE)
 #' 
 #' @examples 
-#' CarReg = lm(formula = speed ~., data = cars)
 #' 
-#' # Standardize coefficients
+#' CarReg = stats::lm(formula = speed ~., data = cars)
+#' 
+#' # standardize
 #' library(lm.beta)
 #' CarRegS = lm.beta::lm.beta(CarReg)
-#' 
-#' # Table format
 #' tidy(CarRegS)
+#'
+#' ## Taken from lm/ lm.beta help
+#' ##
+#' ## Annette Dobson (1990) "An Introduction to Generalized Linear Models".
+#' ## Page 9: Plant Weight Data.
+#' ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
+#' trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
+#' group <- gl(2, 10, 20, labels = c("Ctl","Trt"))
+#' weight <- c(ctl, trt)
+#' lm.D9 <- stats::lm(weight ~ group)
+#' 
+#' # standardize
+#' library(lm.beta)
+#' lm.D9.beta <- lm.beta::lm.beta(lm.D9)
+#' tidy(lm.D9.beta)
 #'
 #' @export
 #' @family lm tidiers
