0dc7d8cf119ff742a09d85bb52016d23f9bbc3fd
Nic <thisisnic@gmail.com>
2018-12-19 08:36:59 +0000

    Fix tidy.survreg tidier failing strict

--- R/survival-survreg-tidiers.R
+++ R/survival-survreg-tidiers.R
@@ -2,8 +2,8 @@
 #' @template title_desc_tidy
 #'
 #' @param x An `survreg` object returned from [survival::survreg()].
-#' @param conf.int Logical indicating whether or not to include a confidence interval in the tidied output. Defaults to FALSE.
-#' @param conf.level The confidence level to use for the confidence interval if conf.int = TRUE. Must be strictly greater than 0 and less than 1. Defaults to 0.95, which corresponds to a 95 percent confidence interval.
+#' @template param_conflevel
+#' @template param_confint
 #' @template param_unused_dots
 #' 
 #' @evalRd return_tidy(regression = TRUE)
