44e57dc1f4d9b312e5f236eb530946f06e8b5407
Matt Lehman <mhlehman24@gmail.com>
2019-01-23 20:55:54 -0500

    update documentation

---
+++ tests/testthat/test-robustbase.R
@@ -0,0 +1,55 @@
+context("robustbase")
+
+skip_if_not_installed("modeltests")
+library(modeltests)
+
+skip_if_not_installed("robustbase")
+library(robustbase)
+
+fit <- lmrob(mpg ~ wt, data = mtcars)
+fit2 <- glmrob(am ~ wt, data = mtcars, family = "binomial")
+
+test_that("robustbase tidier arguments", {
+  check_arguments(tidy.lmrob)
+  check_arguments(glance.lmrob)
+  check_arguments(augment.lmrob)
+  
+  # check_arguments(tidy.glmrob)
+  # check_arguments(glance.glmrob)
+})
+
+test_that("tidy.lmrob", {
+  td <- tidy(fit)
+  check_tidy_output(td)
+})
+
+test_that("glance.lmrob", {
+  gl <- glance(fit)
+  check_glance_outputs(gl)
+})
+
+test_that("augment.lmrob", {
+  check_augment_function(
+    aug = augment.lmrob,
+    model = fit,
+    data = mtcars,
+    newdata = mtcars
+  )
+})
+
+# test_that("tidy.glmrob", {
+#   td <- tidy(fit2)
+#   check_tidy_output(td)
+# })
+# 
+# test_that("glance.glmrob", {
+#   gl <- glance(fit2)
+#   check_glance_outputs(gl)
+# })
+# 
+# test_that("augment.glmrob", {
+#   expect_error(
+#     augment.glmrob(),
+#     "`augment.glmrob` has been removed from broom. See the documentation."
+#   )
+# })
