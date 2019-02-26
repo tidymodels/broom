92437ffbad25f5159eb7df72bfd97c132cfeed45
Jasper Cooper <jaspercooper@gmail.com>
2019-02-26 16:44:00 -0500

    tidy.systemfit is born

---
+++ R/systemfit-tidiers.R
@@ -0,0 +1,52 @@
+#' @templateVar class systemfit
+#' @template title_desc_tidy
+#' 
+#' @param x A `systemfit` object produced by a call to [systemfit::systemfit()].
+#' @template param_confint
+#' @template param_unused_dots
+#' 
+#' @evalRd return_tidy(
+#'   "term", 
+#'   "estimate", 
+#'   "std.error", 
+#'   "mcmc.error", 
+#'   "p.value",
+#'   "conf.low",
+#'   "conf.high"
+#' )
+#'
+#' @details The tibble has four columns. 
+#' @importFrom stats confint
+#' @examples
+#' library(systemfit)
+#' df <- data.frame(X = rnorm(100), Y = rnorm(100), Z = rnorm(100), W = rnorm(100))
+#'
+#` fit <- systemfit(formula = list(Y ~ Z, W ~ X), data = df)
+#'
+#' tidy(fit)
+#' tidy(fit, conf.int = TRUE)
+#' @export
+#' @seealso [tidy()], [systemfit::systemfit()]
+#' @family systemfit tidiers
+#' @aliases systemfit_tidiers
+tidy.systemfit <- function(x, conf.int = TRUE, ...) {
+  nn <- c("term", "estimate", "std.error", "p.value")
+  
+  sf_summary <- summary(x)
+  sf_coefs <- as.data.frame(sf_summary$coefficients)
+  sf_coefs <- sf_coefs[,- which(names(sf_coefs) == "t value")]
+  sf_coefs <- cbind(term = rownames(sf_coefs), sf_coefs)
+  if(conf.int){
+    nn <- c(nn, "conf.low", "conf.high")
+    sf_cis <- confint(x)
+    sf_cis <- matrix(sf_cis,ncol = 2)
+    sf_coefs <- cbind(sf_coefs, sf_cis)
+  }
+  names(sf_coefs) <- nn
+  rownames(sf_coefs) <- NULL
+  
+  ret <- as_broom_tibble(data = sf_coefs)
+  
+  return(ret)
+}
+
