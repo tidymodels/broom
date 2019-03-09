bf4a007720129bde8a344776aec542216feedeb2
François Briatte <briatte@gmail.com>
2019-03-09 18:02:11 +0100

    survey-tidiers: replace finish_glance in glance.svyglm (fixes #572, completes #611)

--- R/survey-tidiers.R
+++ R/survey-tidiers.R
@@ -12,6 +12,9 @@ tidy.svyglm <- tidy.lm # since tidy.glm is itself a copy of tidy.lm
 #' @template title_desc_glance
 #'
 #' @param x A `svyglm` object returned from [survey::svyglm()].
+#' @param maximal A `svyglm` object corresponding to the maximal model against
+#' which to compute the BIC: see Lumley and Scott (2015) for details. Defaults
+#' to \code{x}, which is equivalent to not using a maximal model.
 #' @template param_unused_dots
 #'
 #' @evalRd return_glance(
@@ -49,13 +52,41 @@ tidy.svyglm <- tidy.lm # since tidy.glm is itself a copy of tidy.lm
 #' 
 #' glance(m)
 #'
+#' @references Lumley T, Scott A (2015). AIC and BIC for modelling with complex 
+#' survey data. *Journal of Survey Statistics and Methodology*, 3(1).
+#' <https://doi.org/10.1093/jssam/smu021>.
+#' 
 #' @export
 #' @family lm tidiers
 #' @seealso [survey::svyglm()], [stats::glm()]
-glance.svyglm <- function(x, ...) {
+glance.svyglm <- function(x, maximal = x, ...) {
+  
   s <- survey:::summary.svyglm(x)
   ret <- unrowname(as.data.frame(s[c("null.deviance", "df.null")]))
-  finish_glance(ret, x)
+  
+  # log-likelihood does not apply
+  #
+  # > ret$logLik <- tryCatch(as.numeric(stats::logLik(x)), error = function(e) NULL)
+  # Warning in logLik.svyglm(x) : svyglm not fitted by maximum likelihood.
+  
+  # AIC
+  #
+  ret$AIC <- tryCatch(survey:::AIC.svyglm(mod)["AIC"], error = function(e) NULL)
+  
+  # BIC
+  #
+  ret$BIC <- tryCatch(stats::BIC(x, maximal = maximal)[2], error = function(e) NULL)
+  
+  # deviance
+  #
+  ret$deviance <- tryCatch(stats::deviance(x), error = function(e) NULL)
+  
+  # df.residual
+  #
+  ret$df.residual <- tryCatch(df.residual(x), error = function(e) NULL)
+  
+  as_tibble(ret, rownames = NULL)
+  
 }
 
 #' @rdname ordinal_tidiers
