ea0234f5430485087d6639fd3ff509992bdae6c7
cantabile <charlestigray@gmail.com>
2019-04-04 22:17:46 +1100

    ported @malcolmbarrett's testing script

---
+++ R/rma-tidiers.R
@@ -0,0 +1,178 @@
+#' Tidying methods for meta-analyis objects
+#'
+#' These methods tidy the results of meta-analysis objects
+#'
+#' @param x a meta-analysis object. Currently supports `rma.uni` from the
+#'   `metafor` package.
+#' @param conf.int logical. Include confidence intervals?
+#' @param exponentiate logical. Should the estimates and (if `conf.int` =
+#'   `TRUE`) confidence intervals be exponentiated?
+#' @param include_studies logical. Should individual studies be included in the
+#'    output?
+#' @param ... additional arguments
+#' @param measure measure type. See [metafor::rma()]
+#'
+#' @return a `data.frame`
+#' @export
+#'
+#' @examples
+#'
+#' example_ma %>%
+#'   tidy()
+#'
+#' @rdname tidiers
+tidy.rma <- function(x, conf.int = TRUE, exponentiate = FALSE,
+                     include_studies = TRUE, measure = "GEN", ...) {
+  
+  estimates <- metafor::escalc(yi = x$yi.f, vi = x$vi.f, measure = measure) %>%
+    metafor2df()
+  
+  estimates <- cbind(x$slab, "study", estimates[, c("yi", "sei", "zi")], NA,
+                     estimates[, c("ci.lb", "ci.ub")], stringsAsFactors = FALSE)
+  names(estimates) <- c("study", "type", "estimate", "std.error", "statistic",
+                        "p.value", "conf.low", "conf.high")
+  estimates <- tibble::as_tibble(estimates)
+  
+  betas <- x$beta
+  if (!is.null(nrow(betas)) && nrow(betas) > 1) {
+    # get estimate type and fix spelling
+    study <- rownames(betas)
+    swap <- grepl("intrcpt", study)
+    study[swap] <- "intercept"
+    betas <- as.double(betas)
+  } else {
+    study <- "overall"
+    betas <- betas[1]
+  }
+  
+  results <- tibble::tibble(study = study, type = "summary",
+                            estimate = betas, std.error = x$se,
+                            statistic = x$zval, p.value = x$pval,
+                            conf.low = x$ci.lb, conf.high = x$ci.ub)
+  .data <- if (include_studies) rbind(estimates, results) else results
+  
+  if (exponentiate) {
+    .data$estimate <- exp(.data$estimate)
+    .data$conf.low <- exp(.data$conf.low)
+    .data$conf.high <- exp(.data$conf.high)
+  }
+  
+  if (!conf.int) {
+    .data <- .data[-which(names(.data) %in% c("conf.low", "conf.high"))]
+  }
+  
+  attributes(.data$study) <- NULL
+  
+  tibble::remove_rownames(.data)
+}
+
+#' Construct a one-row summary of meta-analysis model fit statistics.
+#'
+#' `glance()` computes a one-row summary of  meta-analysis objects,
+#'  including estimates of heterogenity and model fit.
+#'
+#' @param x a meta-analysis object. Currently supports `rma.uni` from the
+#'   `metafor` package.
+#' @param ... additional arguments
+#'
+#' @return a `data.frame`
+#' @export
+#'
+#' @examples
+#'
+#' example_ma %>%
+#'   glance()
+#'
+glance.rma <- function(x, ...) {
+  fit_stats <- metafor::fitstats(x)
+  fit_stats <- fit_stats %>%
+    t() %>%
+    as.data.frame()
+  names(fit_stats) <-
+    stringr::str_replace(names(fit_stats), "\\:", "")
+  
+  list(
+    k = x$k,
+    measure = x$measure,
+    method = x$method,
+    i.squared = x$I2,
+    h.squared = x$H2,
+    tau.squared = x$tau2,
+    tau.squared.se = x$se.tau2,
+    QE = x$QE,
+    QE_p = x$QEp,
+    QM = x$QM,
+    QM_p = x$QMp,
+    fit_stats
+  ) %>%
+    # get rid of null values
+    purrr::discard(is.null) %>%
+    # change to tibble with correct column and row names
+    as.data.frame() %>%
+    tibble::as_tibble() %>%
+    tibble::remove_rownames()
+}
+
+#' Augment data with values from a meta-analysis model
+#'
+#' Augment the original data with model residuals, fitted values, and influence
+#' statistics.
+#'
+#' @param x a meta-analysis object. Currently supports `rma.uni` from the
+#'   `metafor` package.
+#' @param ... additional arguments
+#'
+#' @return a `data.frame`
+#' @export
+#'
+#' @examples
+#'
+#' example_ma %>%
+#'   augment()
+#'
+#' @rdname augmenters
+augment.rma <- function(x, ...) {
+  blup0 <- purrr::possibly(metafor::blup, NULL)
+  residuals0 <- purrr::possibly(stats::residuals, NULL)
+  influence0 <- purrr::possibly(stats::influence, NULL)
+  
+  
+  y <- x$yi
+  pred <- blup0(x)
+  if (is.null(pred)) pred <- predict(x)
+  pred <- as.data.frame(pred)
+  
+  # fix names
+  names(pred)[1:4] <- c(".fitted", ".se.fit", "conf.low.fit", "conf.high.fit")
+  credible_intervals <- names(pred) %in% c("cr.lb", "cr.ub")
+  names(pred)[credible_intervals] <- c("cred.low.fit", "cred.high.fit")
+  moderator <- names(pred) == "X"
+  names(pred)[moderator] <- "moderator"
+  
+  res <- residuals0(x)
+  inf <- influence0(x)
+  if (!is.null(inf)) {
+    inf <- cbind(as.data.frame(inf$inf), dfbetas = inf$dfbs$intrcpt)
+    inf <- dplyr::select(
+      inf, .hat = hat, .cooksd = cook.d, .std.resid = rstudent,
+      .dffits = dffits, .cov.ratio = cov.r,
+      tau.squared.del = tau2.del, qe.del = QE.del,
+      .weight = weight, .dfbetas = dfbetas)
+  }
+  
+  ret <- cbind(
+    .rownames = x$slab,
+    y,
+    pred,
+    .resid = res
+  )
+  
+  ret <- tibble::as_tibble(ret) %>%
+    tibble::remove_rownames()
+  
+  if (all(ret$.rownames == seq_along(ret$.rownames))) {
+    ret$.rownames <- NULL
+  }
+  
+  ret
+}
---
+++ tests/testthat/test-rma.R
@@ -0,0 +1,187 @@
+context("rma")
+
+# todo: remove when we make this a proper fork
+
+skip_if_not_installed("metafor")
+library(metafor)
+
+# from the metafor homepage
+dat <-
+  escalc(
+    measure = "RR",
+    ai = tpos,
+    bi = tneg,
+    ci = cpos,
+    di = cneg,
+    data = dat.bcg
+  )
+
+# random effects ----------------------------------------------------------
+res.RE <- rma(yi, vi, data = dat, method = "EB")
+
+
+test_that(("returns tibble for RR"), {
+  check_tidy_output(tidy(res.RE), "data.frame")
+  check_augment_outputs(glance(res.RE), "data.frame")
+  check_glance_output(glance(res.RE))
+})
+
+
+# mixed effects ----------------------------------------------------------
+res.ME <-
+  rma(
+    yi,
+    vi,
+    mods = ~ I(ablat - 33.46),
+    data = dat,
+    method = "EB"
+  )
+
+test_that(("returns tibble for ME"), {
+  check_tidy_output(tidy(res.ME))
+  check_augment_outputs(glance(res.ME))
+  check_glance_output(glance(res.ME))
+})
+
+
+# fixed effects ----------------------------------------------------------
+res.FE <-
+  rma(
+    yi,
+    vi,
+    mods = ~ I(ablat - 33.46),
+    data = dat,
+    method = "FE"
+  )
+
+test_that(("returns tibble for FE"), {
+  check_tidy_output(tidy(res.FE))
+  check_augment_outputs(glance(res.FE))
+  check_glance_output(glance(res.FE))
+})
+
+
+# Weighted Regression Analysis ---------------------------------------------
+
+dat2 <- data.frame(
+  id = c(100, 308, 1596, 2479, 9021, 9028, 161, 172, 537, 7049),
+  yi = c(-0.33, 0.32, 0.39, 0.31, 0.17, 0.64, -0.33, 0.15, -0.02, 0.00),
+  vi = c(0.084, 0.035, 0.017, 0.034, 0.072, 0.117, 0.102, 0.093, 0.012, 0.067),
+  random = c(0, 0, 0, 0, 0, 0, 1, 1, 1, 1),
+  intensity = c(7, 3, 7, 5, 7, 7, 4, 4, 5, 6)
+)
+
+res.WFE <- rma(yi, vi, mods = ~random + intensity, data = dat2, method = "FE")
+
+test_that(("returns tibble for weighted FE"), {
+  check_tidy_output(tidy(res.WFE))
+  check_augment_outputs(glance(res.WFE))
+  check_glance_output(glance(res.WFE))
+})
+
+
+# multivariate models -----------------------------------------------------
+
+dat.long <-
+  to.long(
+    measure = "OR",
+    ai = tpos,
+    bi = tneg,
+    ci = cpos,
+    di = cneg,
+    data = dat.colditz1994
+  )
+dat.long <-
+  escalc(
+    measure = "PLO",
+    xi = out1,
+    mi = out2,
+    data = dat.long
+  )
+dat.long$tpos <-
+  dat.long$tneg <- dat.long$cpos <- dat.long$cneg <- NULL
+levels(dat.long$group) <- c("EXP", "CON")
+dat.long$group <- relevel(dat.long$group, ref = "CON")
+
+res.MV <-
+  rma.mv(
+    yi,
+    vi,
+    mods = ~ group - 1,
+    random = ~ group |
+      trial,
+    struct = "UN",
+    data = dat.long,
+    method = "ML"
+  )
+
+test_that(("returns tibble for MV"), {
+  check_tidy_output(tidy(res.MV))
+  check_augment_outputs(glance(res.MV))
+  check_glance_output(glance(res.MV))
+})
+
+
+# GLMM --------------------------------------------------------------------
+
+res.GLMM <-
+  rma.glmm(
+    measure = "PLO",
+    xi = ci,
+    ni = n2i,
+    data = dat.nielweise2007
+  )
+
+test_that(("returns tibble for GLMM"), {
+  check_tidy_output(tidy(res.GLMM))
+  # check_augment_outputs(glance(res.GLMM))
+  check_glance_outputs(glance(res.GLMM))
+})
+
+
+# Peto method -------------------------------------------------------------
+
+dat.yusuf1985$grp_ratios <-
+  round(dat.yusuf1985$n1i / dat.yusuf1985$n2i, 2)
+res.peto <- rma.peto(
+  ai = ai,
+  n1i = n1i,
+  ci = ci,
+  n2i = n2i,
+  data = dat.yusuf1985,
+  subset = (table == "6")
+)
+
+test_that(("returns tibble for peto"), {
+  check_tidy_output(tidy(res.peto))
+  # check_augment_outputs(glance(res.peto))
+  check_glance_output(glance(res.peto))
+})
+
+# Mantel-Haenszel Method for the Risk Difference --------------------------
+
+dat <- data.frame(
+  age = c("Age <55", "Age 55+"),
+  ai = c(8, 22),
+  bi = c(98, 76),
+  ci = c(5, 16),
+  di = c(115, 69)
+)
+
+res.MH <-
+  rma.mh(
+    ai = ai,
+    bi = bi,
+    ci = ci,
+    di = di,
+    data = dat,
+    measure = "RD",
+    digits = 3,
+    level = 90
+  )
+
+test_that(("returns tibble for MH"), {
+  check_tidy_output(tidy(res.MH))
+  # check_augment_outputs(glance(res.MH))
+  check_glance_outputs(glance(res.MH))
+})
