481adf0234f73a481c794e88c9b513c045b43bf3
alex hayes <alexpghayes@gmail.com>
2019-04-08 22:32:54 -0500

    Add tidy.speedglm() and glance.speedglm()

---
+++ tests/testthat/test-speedglm-speedglm.R
@@ -0,0 +1,31 @@
+context("test-speedglm-speedglm")
+
+skip_if_not_installed("modeltests")
+library(modeltests)
+
+skip_if_not_installed("speedglm")
+library(speedglm)
+
+clotting <- data.frame(
+  u = c(5, 10, 15, 20, 30, 40, 60, 80, 100),
+  lot1 = c(118, 58, 42, 35, 27, 25, 21, 19, 18)
+)
+
+fit <- speedglm(lot1 ~ log(u), data = clotting, family = Gamma(log))
+
+test_that("speedglm tidiers arguments", {
+  check_arguments(tidy.speedglm)
+  check_arguments(glance.speedglm)
+})
+
+test_that("tidy.speedglm", {
+  td <- tidy(fit, conf.int = TRUE, exponentiate = TRUE)
+  check_tidy_output(td)
+  check_dims(td, 2, 7)
+})
+
+test_that("glance.speedglm", {
+  gl <- glance(fit)
+  check_glance_outputs(gl)
+})
+
