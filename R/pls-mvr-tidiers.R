3e0f8113d508e234d98ca1dbf0a3106d8ee8cee4
Yusuke Kawashima <kawashimayusuke113@gmail.com>
2019-09-01 14:33:11 +0900

    add mvr tidier

---
+++ R/pls-mvr-tidiers.R
@@ -0,0 +1,162 @@
+#' @templateVar class mvr
+#' @template title_desc_tidy
+#'
+#' @param x An `mvr` object such as those created by [pls::mvr()], [pls::plsr()], [pls::cpplsr()] and [pls::pcr()].
+#' @param ncomp The number of components to include in the model. Ignored if comps is specified.
+#' @param comps If specified, the values of each component out of comps are shown.
+#' @param intercept Whether coefficients for the intercept should be included. Ignored if comps is specified.
+#' @template param_unused_dots
+#'
+#' @evalRd return_tidy(
+#'   "response",
+#'   "term",
+#'   "estimate"
+#' )
+#'
+#' @details
+#' The number of components to fit is specified with the argument ncomp.
+#' It means from the 1st component to the ncomp-th component are used to fit.
+#' If comps is given, however, estimates are the coefficients for a model with only the component comps[i], i.e. the contribution of the component comps[i] on the regression coefficients.
+#'
+#' @family mvr tidiers
+#'
+#' @examples
+#' library(pls)
+#' data(yarn) # Single-response model
+#' yarn.pls <- plsr(density ~ NIR, 6, data = yarn, validation = "CV")
+#' glance(yarn.pls)
+#' tidy(yarn.pls)
+#' augment(yarn.pls)
+#'
+#' data(oliveoil) # Multi-response model
+#' sens.pls <- plsr(sensory ~ chemical, ncomp = 4, scale = TRUE, data = oliveoil)
+#' glance(sens.pls)
+#' tidy(sens.pls)
+#' augment(sens.pls)
+#'
+#' set.seed(42)
+#' test_id <- sample(1:nrow(yarn), nrow(yarn) / 3)
+#' yarn.pls.train <- plsr(density ~ NIR, 3, data = slice(yarn, -test_id))
+#' augment(yarn.pls.train, newdata = slice(yarn, test_id)) %>%
+#'   mutate(.rownames = test_id) %>%
+#'   ggplot(aes(.rownames, .t.squared[, 3])) +
+#'   geom_point() +
+#'   ggtitle("How far each sample is from the center of the model.")
+#' @export
+#'
+#' @seealso [tidy()], [pls::coef.mvr()]
+#'
+tidy.mvr <- function(x, ncomp = x$ncomp, comps = NULL, intercept = FALSE, ...) {
+  comp_type <- ifelse(is.null(comps), "ncomp", "comps")
+  if (comp_type == "comps") {
+    if (length(comps) > 1) {
+      line <- paste("comps is a multi-component vector:",
+                    "comps = {comps[1]} will be used.")
+      message(glue(line))
+    }
+  } else {
+    if (length(ncomp) > 1) {
+      line <- paste("ncomp is a multi-component vector:",
+                    "ncomp = {ncomp[1]} will be used.")
+      message(glue(line))
+    }
+  }
+  ret <- coef(x, ncomp = ncomp[1], comps = comps[1], intercept = intercept, ...)
+  ret <- apply(ret, 3, fix_data_frame)[[1]]
+  ret <- tidyr::gather(ret, key = "response", value = "estimate", -"term")
+  dplyr::select(ret, "response", "term", "estimate")
+}
+
+#' @templateVar class mvr
+#' @template title_desc_augment
+#'
+#' @param x a mvr object such as those created by [pls::mvr()], [pls::plsr()], [pls::cpplsr()] and [pls::pcr()].
+#' @template param_data
+#' @template param_newdata
+#' @param ncomp The number of components to include in the model. Ignored if comps is specified.
+#' @template param_unused_dots
+#'
+#' @evalRd return_tidy(
+#'   ".rownames",
+#'   "response",
+#'   ".fitted",
+#'   .t.squared="Hotelling's T^2 value."
+#' )
+#'
+#' @inherit tidy.mvr examples
+#' 
+#' @family mvr tidiers
+#'
+#' @export
+#'
+#' @seealso [augment()], [pls:::predict.mvr()]
+#'
+augment.mvr <- function(x, 
+                        data = model.frame(x),
+                        newdata = NULL, 
+                        ncomp = x$ncomp, ...) {
+  if (length(ncomp) > 1) {
+    line <- paste("ncomp is a multi-component vector:",
+                  "ncomp = {ncomp[1]} will be used.")
+    message(glue(line))
+  }
+  temp <- x
+  class(temp) <- c("mvr_temp", class(temp))
+  df <- augment_newdata(temp, 
+                        data, 
+                        newdata, 
+                        FALSE, 
+                        ncomp = ncomp[1], 
+                        type = "response", ...)
+  df$.scores <- predict(x, newdata = newdata, type = "scores", ...)
+
+  t2 <- as.data.frame(df$.scores^2)
+  t2 <- purrr::map2(t2, x$Xvar, ~ .x / .y)
+  t2 <- purrr::accumulate(t2, `+`)
+  t2 <- as.data.frame(t2)
+  t2 <- tibble::tidy_names(t2)
+  t2 <- as.matrix(t2)
+  df$.t.squared <- t2
+
+  df
+}
+
+# Make predict(), an S3 method, preferable in augment_newdata() using a temporary class
+predict.mvr_temp <- function(object, ncomp, type, ...) {
+  predict(object, ncomp = ncomp, type = type, ...)[, , 1]
+}
+
+#' @templateVar class mvr
+#' @template title_desc_glance
+#'
+#' @param x An `mvr` object such as those created by [pls::mvr()], [pls::plsr()], [pls::cpplsr()] and [pls::pcr()].
+#' @param ncomp The number of components to include in the model. Ignored if comps is specified.
+#' @template param_unused_dots
+#'
+#' @evalRd return_glance(
+#'   "r.squared"
+#' )
+#'
+#' @inherit tidy.mvr examples
+#'
+#' @family mvr tidiers
+#'
+#' @export
+#'
+#' @seealso [glance()], [pls::R2()]
+#'
+glance.mvr <- function(x, ncomp = x$ncomp, ...) {
+  if (length(ncomp) > 1) {
+    line <- paste("ncomp is a multi-component vector:",
+                  "ncomp = {ncomp[1]} will be used.")
+    message(glue(line))
+  }
+  r2 <- pls::R2(x, ncomp = ncomp[1])$val
+  response_names <- dimnames(r2)$response
+  r2 <- r2[, , 2]
+  names(r2) <- response_names
+  r2 <- matrix(r2, ncol = length(r2), dimnames = list(NULL, names(r2)))
+  ret <- tibble::tibble(r.squared = 1)
+  ret$r.squared <- r2
+  ret
+}
---
+++ tests/testthat/test-pls-mvr.R
@@ -0,0 +1,58 @@
+context("pls-mvr")
+
+skip_if_not_installed("modeltests")
+library(modeltests)
+
+skip_if_not_installed("pls")
+library(pls)
+library(modeltests)
+
+data(yarn)
+yarn_pcr <- pcr(density ~ NIR, 6, data = yarn, validation = "CV")
+yarn_pls <- plsr(density ~ NIR, 5, data = yarn, validation = "CV")
+yarn_cppls <- cppls(density ~ NIR, 6, data = yarn, validation = "CV")
+
+data(oliveoil)
+sens_pcr <- pcr(sensory ~ chemical, ncomp = 4, scale = TRUE, data = oliveoil)
+sens_pls <- plsr(sensory ~ chemical, ncomp = 4, scale = TRUE, data = oliveoil)
+
+test_that("mvr tidier arguments", {
+  check_arguments(tidy.mvr, strict = FALSE)
+  check_arguments(augment.mvr, strict = FALSE)
+  check_arguments(glance.mvr, strict = FALSE)
+})
+
+test_that("tidy.mvr", {
+  tidy_yarn_pcr <- tidy(yarn_pcr)
+  tidy_yarn_pls <- tidy(yarn_pls)
+  tidy_yarn_cppls <- tidy(yarn_cppls)
+  tidy_sens_pcr <- tidy(sens_pcr)
+  tidy_sens_pls <- tidy(sens_pls)
+
+  check_tidy_output(tidy_yarn_pcr)
+  check_tidy_output(tidy_yarn_pls)
+  check_tidy_output(tidy_yarn_cppls)
+  check_tidy_output(tidy_sens_pcr)
+  check_tidy_output(tidy_sens_pls)
+})
+
+test_that("augment.mvr", {
+  check_augment_function(
+    aug = augment.mvr,
+    model = yarn_pls,
+    data = yarn,
+    newdata = yarn,
+    strict = FALSE
+  )
+
+  au <- augment(yarn_pls)
+  expect_true(all(c(".resid", ".fitted", ".t.squared", ".scores") %in% names(au)))
+})
+
+test_that("glance.mvr", {
+  gl <- glance(yarn_pls)
+  gl2 <- glance(sens_pls)
+
+  check_glance_outputs(gl)
+  check_glance_outputs(gl2)
+})
