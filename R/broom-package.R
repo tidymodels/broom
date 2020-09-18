d8269d0f52348a5323d2d496102a98a78624b1bc
Simon P. Couch <simonpatrickcouch@gmail.com>
2020-09-17 18:08:39 -0700

    re-export function generics only once (closes #936)
    the generics were re-exported twice from modelgenerics! (the other set is in broom.R--left that one intact.)

--- R/broom-package.R
+++ R/broom-package.R
@@ -28,17 +28,3 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' @importFrom generics augment
-#' @export
-#' @seealso [augment.lm()]
-generics::augment
-
-#' @importFrom generics tidy
-#' @export
-#' @seealso [tidy.lm()]
-generics::tidy
-
-#' @importFrom generics glance
-#' @export
-#' @seealso [glance.lm()]
-generics::glance
