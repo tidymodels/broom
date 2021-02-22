c0c43412bc98db61a0af03141287420c6f92347b
joshyam-k <yamamojo@reed.edu>
2021-02-22 15:52:11 -0800

    Added tidy and glance files for glm.nb and roxygen2 documentation for both as well. Also wrote unit tests although one is not passing rn.

---
+++ R/test-mass-negbin.R
@@ -0,0 +1,33 @@
+context("mass-negbin")
+
+skip_if_not_installed("modeltests")
+library(modeltests)
+
+skip_if_not_installed("MASS")
+library(MASS)
+
+fit <- glm.nb(Days ~ Sex/(Age + Eth*Lrn), data = MASS::quine)
+
+test_that("MASS::glm.nb tidier arguments", {
+  check_arguments(tidy.negbin)
+  check_arguments(glance.negbin)
+})
+
+test_that("tidy.negbin", {
+  td1 <- tidy(fit)
+  td2 <- tidy(fit, conf.int = TRUE)
+  
+  check_tidy_output(td2)
+  check_tidy_output(td1)
+  
+  expect_false(NA %in% td2$conf.low)
+  expect_false(NA %in% td2$conf.high)
+})
+
+test_that("glance.negbin", {
+  gl <- glance(fit)
+  check_glance_outputs(gl)
+  check_dims(gl, 1, 8)
+})
+
+
