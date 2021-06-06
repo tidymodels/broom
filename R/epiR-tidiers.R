fe7695258d33f2bc76a425ee7a400e15f366de4c
Alex Hayes <alexpghayes@gmail.com>
2021-06-06 13:13:39 -0500

    Style changes

--- R/epiR-tidiers.R
+++ R/epiR-tidiers.R
@@ -20,38 +20,48 @@
 #'   or tests contained in `massoc` when [epiR::epi.2by2()] is called.
 #'
 #' @examples
+#' 
 #' library(epiR)
+#' 
 #' dat <- matrix(c(13, 2163, 5, 3349), nrow = 2, byrow = TRUE)
+#' 
 #' rownames(dat) <- c("DF+", "DF-")
 #' colnames(dat) <- c("FUS+", "FUS-")
+#' 
 #' fit <- epi.2by2(
 #'   dat = as.table(dat), method = "cross.sectional",
 #'   conf.level = 0.95, units = 100, outcome = "as.columns"
 #' )
 #'
 #' tidy(fit, parameters = "moa")
+#' 
 #' @export
 #' @seealso [tidy()], [epiR::epi.2by2()]
 #' @family epiR tidiers
 #' @aliases epiR_tidiers
 tidy.epi.2by2 <- function(x, parameters = c("moa", "stat"), ...) {
+  
   s <- summary(x, ...)
   method <- rlang::arg_match(parameters)
   nm <- names(x$massoc)
 
   if (method == "moa") {
     keep <- c("measure", "est", "lower", "upper")
-    tibble::tibble(s, measure = nm) %>%
+    
+    out <- tibble::tibble(s, measure = nm) %>%
       tidyr::unnest(cols = s) %>%
       dplyr::filter(!is.na(.$est)) %>%
       dplyr::select(keep) %>%
-      dplyr::rename_all(~ c("term", "estimate", "conf.low", "conf.high"))
-  } else if (method == "stat") {
-    keep <- c("measure", "test.statistic", "df", "p.value")
-    tibble::tibble(s, measure = nm) %>%
-      tidyr::unnest(cols = s) %>%
-      dplyr::filter(!is.na(.$test.statistic)) %>%
-      dplyr::select(keep) %>%
-      dplyr::rename_all(~ c("term", "statistic", "df", "p.value"))
-  }
+      dplyr::rename_all(c("term", "estimate", "conf.low", "conf.high"))
+    
+    return(out)
+  } 
+  
+  keep <- c("measure", "test.statistic", "df", "p.value")
+  
+  tibble::tibble(s, measure = nm) %>%
+    tidyr::unnest(cols = s) %>%
+    dplyr::filter(!is.na(.$test.statistic)) %>%
+    dplyr::select(keep) %>%
+    dplyr::rename_all(c("term", "statistic", "df", "p.value"))
 }
