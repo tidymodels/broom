6dce9daf01c286d0a95aa468b42eda4f858398da
Simon P. Couch <simonpatrickcouch@gmail.com>
2021-10-16 19:37:24 -0400

    run `mclust` examples conditionally

--- R/mclust-tidiers.R
+++ R/mclust-tidiers.R
@@ -17,7 +17,8 @@
 #' )
 #'
 #' @examples
-#'
+#' 
+#' if (requireNamespace("mclust", quietly = TRUE)) {
 #' library(dplyr)
 #' library(mclust)
 #' set.seed(27)
@@ -42,6 +43,7 @@
 #' tidy(m)
 #' augment(m, points)
 #' glance(m)
+#' }
 #' @export
 #' @aliases mclust_tidiers
 #' @seealso [tidy()], [mclust::Mclust()]
