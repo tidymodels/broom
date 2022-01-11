f550149ec1deee9be9673b3b5c93d2e3138a358a
Simon P. Couch <simonpatrickcouch@gmail.com>
2022-01-11 15:47:01 -0800

    update list tidier description
    more grammar + re`document`

--- man-roxygen/title_desc_tidy_list.R
+++ man-roxygen/title_desc_tidy_list.R
@@ -7,9 +7,9 @@
 #'   dispatch.
 #' 
 #'   These functions look at the elements of a list and determine if there is
-#'   an appropriate tidying method to apply to the list. Those tidiers are
-#'   themselves that are implemented as functions of the form `tidy_<function>`
-#'   or `glance_<function>` and that are not exported (but they are documented!).
+#'   an appropriate tidying method to apply to the list. Those tidiers are 
+#'   implemented as functions of the form `tidy_<function>` or 
+#'   `glance_<function>` and are not exported (but they are documented!).
 #' 
 #'   If no appropriate tidying method is found, they throw an error.
 #' 
