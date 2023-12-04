fc19479339fc6b3f2abd31baa718a4e486613b62
olivroy <olivierroy71@hotmail.com>
2023-12-04 10:37:31 -0500

    Revert

--- R/broom-package.R
+++ R/broom-package.R
@@ -1,3 +1,13 @@
+# custom package description for the package help file.
+#' @description
+#' Convert statistical analysis objects from R into tidy tibbles,
+#' so that they can more easily be combined, reshaped and otherwise processed
+#' with tools like dplyr, tidyr and ggplot2. The package provides three S3
+#' generics: tidy, which summarizes a model's statistical findings such as
+#' coefficients of a regression; augment, which adds columns to the original
+#' data such as predictions, residuals and cluster assignments; and glance,
+#' which provides a one-row summary of model-level statistics.
+#' 
 #' @keywords internal
 "_PACKAGE"
 
