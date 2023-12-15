f4fc69d6a951298045364c549e2c3b294188503f
olivroy <52606734+olivroy@users.noreply.github.com>
2023-12-15 10:41:51 -0500

    deduplicate package-level docs (#1184)

--- R/broom-package.R
+++ R/broom-package.R
@@ -1,13 +1,18 @@
-#' @title Convert Statistical Objects into Tidy Tibbles
+# Custom package description for the package help file.
 #' @name broom
-#' @description Convert statistical analysis objects from R into tidy tibbles,
-#'   so that they can more easily be combined, reshaped and otherwise processed
-#'   with tools like dplyr, tidyr and ggplot2. The package provides three S3
-#'   generics: tidy, which summarizes a model's statistical findings such as
-#'   coefficients of a regression; augment, which adds columns to the original
-#'   data such as predictions, residuals and cluster assignments; and glance,
-#'   which provides a one-row summary of model-level statistics.
-#'
+#' @description
+#' Convert statistical analysis objects from R into tidy tibbles,
+#' so that they can more easily be combined, reshaped and otherwise processed
+#' with tools like dplyr, tidyr and ggplot2. The package provides three S3
+#' generics: tidy, which summarizes a model's statistical findings such as
+#' coefficients of a regression; augment, which adds columns to the original
+#' data such as predictions, residuals and cluster assignments; and glance,
+#' which provides a one-row summary of model-level statistics.
+#' 
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @importFrom stats AIC BIC coef confint fitted logLik model.frame
 #' @importFrom stats pnorm qnorm qt predict residuals setNames var
 #' @importFrom stats quantile model.response terms na.pass na.omit
@@ -20,8 +25,5 @@
 #' @importFrom glue glue
 #'
 #' @import dplyr
-#'
-#' @docType package
-#' @aliases broom broom-package
-#' @keywords internal
-"_PACKAGE"
+## usethis namespace: end
+NULL
