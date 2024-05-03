edc5d31adfc6536413fbc25f6d8775ae836f4139
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2024-05-03 21:18:16 +0200

    test that ellipses are passed to `summary.betareg()`

--- tests/testthat/test-betareg.R
+++ tests/testthat/test-betareg.R
@@ -32,6 +32,24 @@ test_that("tidy.betareg", {
   )
 
   check_dims(td1, 12, 8)
+  
+  #test passing ellipsis to summary
+  data_test <- structure(
+    list(p = c(0.4238, 0.8248, 0.5927, 0.3208, 0.8317, 
+               0.7314, 0.4624, 0.5224, 0.3528, 0.7739, 0.1264, 0.4516, 0.4306, 
+               0.5243, 0.5117, 0.149, 0.3342, 0.5069, 0.7101, 0.8019, 0.7569, 
+               0.9096, 0.9013, 0.5403, 0.2264, 0.5775, 0.7366, 0.4086, 0.575, 
+               0.6623, 0.7758, 0.1517, 0.7587, 0.3247, 0.7463, 0.6325), 
+         w = structure(c(1L, 2L, 9L, 4L, 3L, 5L, 1L, 9L, 4L, 3L, 5L, 2L, 4L, 5L, 1L, 2L, 9L, 
+                         4L, 3L, 5L, 8L, 7L, 6L, 1L, 2L, 9L, 3L, 5L, 8L, 7L, 1L, 2L, 9L, 
+                         4L, 3L, 5L), 
+                       levels = c("1", "2", "3", "4", "5", "6", "7", "8", "9"), 
+                       class = "factor")), 
+    row.names = c(NA, -36L), class = c("tbl_df", "tbl", "data.frame")
+  )
+  m <- betareg::betareg(p ~ w, data = data_test)
+  expect_warning(tidy(m))
+  expect_silent(tidy(m, type = "sweighted"))
 })
 
 test_that("glance.betareg", {
