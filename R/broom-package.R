35a451a507fdb6d1b2dae482dc3a34414e74e881
simonpcouch <simonpatrickcouch@gmail.com>
2024-09-26 10:37:36 -0500

    import `nobs()` generic

--- R/broom-package.R
+++ R/broom-package.R
@@ -16,7 +16,7 @@
 #' @importFrom stats AIC BIC coef confint fitted logLik model.frame
 #' @importFrom stats pnorm qnorm qt predict residuals setNames var
 #' @importFrom stats quantile model.response terms na.pass na.omit
-#' @importFrom stats influence rstandard cooks.distance
+#' @importFrom stats influence rstandard cooks.distance nobs
 #' @importFrom purrr map_df set_names possibly
 #' @importFrom tibble tibble as_tibble
 #' @importFrom tidyr pivot_longer pivot_wider
